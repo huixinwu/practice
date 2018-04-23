42ebd6a2789104244bfeaad59ba11657f5040e1a
huixinwu <38544670+huixinwu@users.noreply.github.com>
2018-04-23 19:12:07 -0400

    Add files via upload

---
+++ cordova/js/cordova.js
@@ -0,0 +1,12 @@
+function myDisplay1() {
+    document.getElementById('textDisplay').innerHTML ='Red brings text and images to the foreground.Use it as an accent color to stimulate people to make quick decisions; it is a perfect color for Buy Now or Click Here buttons on Internet banners and websites.';
+    }
+function myDisplay2(){
+  document.getElementById('textDisplay').innerHTML='This color is also commonly associated with energy, so you can use it when promoting energy drinks, games, cars, items related to sports and high physical activity.';
+}
+function imageText1(){
+  document.getElementsByClassName('imgLeft').innerHTML='Red is the color of fire and blood, so it is associated with energy, war, danger, strength, power, determination as well as passion, desire, and love.';
+}
+function imageText2(){
+  document.getElementsByClassName('imgRight').innerHTML='In advertising, red is often used to evoke erotic feelings (red lips, red nails, red-light districts, Lady in Red, etc). Red is widely used to indicate danger (high voltage signs, traffic lights).'
+}
